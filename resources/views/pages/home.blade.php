3129110222c16a288273710a545c029ea968a0b4
Nathan Watts <nathan@naykel.com.au>
2024-01-16 16:45:08 +1000

    install gotime

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+<x-gotime-app-layout layout="{{ config('naykel.template') }}" hasContainer class="py-5-3-2-2">
+
+    <h1>{{ $pageTitle ?? null }}</h1>
+
+
+</x-gotime-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Naykel\Gotime\RouteBuilder;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.home');
+})->name('home');
+
+(new RouteBuilder('nav-main'))->create();
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+*/
+
+// (new RouteBuilder('nav-admin'))->create();
+
+// Route::middleware(['role:super|admin', 'auth'])->prefix('admin')->name('admin')->group(function () {
+//     Route::view('/', 'gotime::admin.dashboard'); // admin dashboard
+// });
