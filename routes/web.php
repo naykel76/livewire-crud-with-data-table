3a1d05d36fb0a215de41537fc705b1c111046849
Nathan Watts <nathan@naykel.com.au>
2024-01-17 12:05:32 +1000

    housekeeping

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Naykel\Gotime\RouteBuilder;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-*/
 
 Route::get('/', function () {
     return view('pages.home');
 })->name('home');
-
-(new RouteBuilder('nav-main'))->create();
-
-/*
-|--------------------------------------------------------------------------
-| Admin Routes
-|--------------------------------------------------------------------------
-|
-*/
-
-// (new RouteBuilder('nav-admin'))->create();
-
-// Route::middleware(['role:super|admin', 'auth'])->prefix('admin')->name('admin')->group(function () {
-//     Route::view('/', 'gotime::admin.dashboard'); // admin dashboard
-// });
